7d003a8fec4ff6bdc0884d37829dfe252cb283cf
anandvsinghm <anand_v.singh@outlook.com>
2024-01-12 14:24:32 +0530

    Git and nvimtree bindings

---
+++ after/plugin/nvimtree.lua
@@ -0,0 +1,5 @@
+
+vim.keymap.set("n", "<leader>nn", "<cmd>NvimTreeToggle<CR>");
+vim.keymap.set("n", "<leader>ntf", "<cmd>NvimTreeFocus<CR>");
+vim.keymap.set("n", "<leader>nff", "<cmd>NvimTreeFindFile<CR>");
+vim.keymap.set("n", "<leader>nc", "<cmd>NvimTreeCollapse<CR>");
