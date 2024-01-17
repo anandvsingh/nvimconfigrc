edfc0414c334ff7f229d3de8753b2aa1e2e36de9
anandvsinghm <anand_v.singh@outlook.com>
2024-01-17 17:38:49 +0530

    Updated git diff

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,3 +1,3 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 vim.keymap.set("n", "<leader>gb", "<cmd>Git blame<CR>");
-vim.keymap.set("n", "<leader>gb", "<cmd>Gvdiffsplit<CR>");
+vim.keymap.set("n", "<leader>gd", "<cmd>Gvdiffsplit<CR>");
